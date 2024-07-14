eb0399b8fcfa9688c890566e8d4b90c19e2b858f
ZainicDey <119753864+ZainicDey@users.noreply.github.com>
2024-07-14 23:58:35 +0600

    Update settings.py

--- mangoshop/settings.py
+++ mangoshop/settings.py
@@ -34,16 +34,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-
 # Application definition
 
 INSTALLED_APPS = [

232103c8c994eaf652813f5e17a135f5cbb0967d
ZainicDey <119753864+ZainicDey@users.noreply.github.com>
2024-07-14 23:55:30 +0600

    Update views.py

--- mypp/views.py
+++ mypp/views.py
@@ -141,29 +141,26 @@ def activate(request, uid64, token):
 
 class UserLoginApiView(APIView):
     permission_classes = [AllowAny]
-
     def post(self, request):
-        serializer = serializers.UserLoginSerializer(data=request.data)
-        
+        serializer = serializers.UserLoginSerializer(data = self.request.data)
         if serializer.is_valid():
             username = serializer.validated_data['username']
             password = serializer.validated_data['password']
 
-            user = authenticate(username=username, password=password)
+            user = authenticate(username= username, password=password)
             
             if user:
-                login(request, user)  # Log in the user
                 token, _ = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key, 'user_id': user.id})
+                print(token)
+                print(_)
+                login(request, user)
+                return Response({'token' : token.key, 'user_id' : user.id})
             else:
-                return Response({'error': "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-                
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-class UserLogoutView(APIView):
-    permission_classes = [IsAuthenticated]
+                return Response({'error' : "Invalid Credential"})
+        return Response(serializer.errors)
 
-    def post(self, request):
-        request.user.auth_token.delete()  # Delete the token
-        logout(request)  # Log out the user
-        return Response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
+class UserLogoutView(APIView):
+    def get(self, request):
+        request.user.auth_token.delete()
+        logout(request)
+        return redirect('login')

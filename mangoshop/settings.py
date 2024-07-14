01f46ee4d905bb427400f3dfaac63334d3ad4e6e
ZainicDey <119753864+ZainicDey@users.noreply.github.com>
2024-07-15 00:58:09 +0600

    Update settings.py

--- mangoshop/settings.py
+++ mangoshop/settings.py
@@ -30,6 +30,7 @@ CSRF_TRUSTED_ORIGINS = ['https://mangoshopbd.onrender.com','https://*.127.0.0.1'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
